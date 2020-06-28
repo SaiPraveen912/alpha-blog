c19b1fc790810d1cafa220d222c76857efa3de12
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-06-28 11:01:36 -0400

    Added validations to article.rb file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 # Created a article.rb file in models section
 # This model gives us setters and getteres so that we can work with individual articles data.
 class Article < ApplicationRecord
-
+    # Added validations to models->article.rb
+    validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 300 }
 end
