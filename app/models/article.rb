8f1781937c36161de9941ed0e500fe1b280ac2f4
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-06-28 10:40:25 -0400

    Created a article.rb file in models section

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+# Created a article.rb file in models section
+# This model gives us setters and getteres so that we can work with individual articles data.
+class Article < ApplicationRecord
+
+end
