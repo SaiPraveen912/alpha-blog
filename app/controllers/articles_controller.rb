b7312f21f760c726af56794f853f8fdff52cff78
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-07-02 10:50:00 -0400

    Added validation and flash messages when creating new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,18 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
+        # Save article to DB and then can view on articles listing page. 
         @article = Article.new(params.require(:article).permit(:title, :description))  # white listing
-        @article.save
-        redirect_to @article  # Where we want it to go after saving to DB.
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article  # Where we want it to go after saving to DB.
+        else
+            render 'new'
+        end
     end
 
 end
