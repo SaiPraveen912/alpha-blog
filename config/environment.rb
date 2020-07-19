c9e73b1068c0c57f97889e819b1cb5ee7d68b8b8
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-07-19 04:20:17 -0400

    Styling show view using bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# The below code disables field with errors provided by rails.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
