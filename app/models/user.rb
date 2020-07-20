98a8eddebca8a6b646af1fa0b25c824ece2c147e
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-07-19 23:10:37 -0400

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+                                  length: { minimum: 3, maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i   # Validation of email using regular expression defined with a constant.
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                                  length: { maximum: 105 }, 
+                                  format: { with: VALID_EMAIL_REGEX }
 end
