a4f4952e615333fc5014c06a1bbac5a52ca98af9
SaiPraveen912 <saipraveen.immanni@gmail.com>
2020-08-11 01:32:43 -0400

    Sessions and authentication helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]   # memoization if current_user does not exist it will hit DB once and save the instance
+  end
+
+  def logged_in?
+    !!current_user       # !! is used to turn to boolean values true or false
+  end
+
 end
